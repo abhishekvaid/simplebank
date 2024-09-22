12cbaf0e454b591a36a0a609487e6179b9539fd6
Abhishek Vaid <abhishekvaid@clipboardhealth.com>
2024-09-22 15:27:01 -0400

    Added compose file

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e  # will exit the script immediately if any of the command fails 
+
+echo "Starting the application..."
+echo $DRIVER_SOURCE
+
+/usr/bin/migrate -path=db/migration -database=$DRIVER_SOURCE up
+
+exec "$@" # hand over the execution to command passed, In our case, ./app/simple_bank app 
